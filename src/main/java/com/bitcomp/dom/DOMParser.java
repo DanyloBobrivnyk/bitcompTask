f4959242bec744a67aae68b1fcda2696c2a3210b
dBobrivnyk <hoxtber@gmail.com>
2021-05-17 16:22:35 +0200

    "DOM Parsing. JUnit test added for program console output"

--- src/main/java/com/bitcomp/dom/DOMParser.java
+++ src/main/java/com/bitcomp/dom/DOMParser.java
@@ -7,11 +7,25 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import java.util.HashMap;
 
 public class DOMParser {
 
-    public static void parse(Document doc, ParserFormat parserFormat)
+    public static void parse(String source, String formatName) throws Exception
+    {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(source);
+
+        ParserFormat parserFormat = new ParserFormat(formatName);
+        DOMParser.parse(doc, parserFormat);
+    }
+
+    private static void parse(Document doc, ParserFormat parserFormat)
     {
         NodeList list = doc.getElementsByTagName(parserFormat.getParentNodeName());
 
@@ -21,9 +35,10 @@ public class DOMParser {
             if(nodeItem.getNodeType() == Node.ELEMENT_NODE)
             {
                 Element element = (Element) nodeItem;
-                //may be thrown exception
+                //TODO: Check for some exceptions
                 HashMap<ParserFields, String> childNodesHashMap = parserFormat.getParentNodeChildNodes();
 
+                //Write all the fields from hashmap to the console.
                 for(ParserFields p : ParserFields.values())
                 {
                     if(childNodesHashMap.get(p) != null)
---
+++ src/test/java/com/bitcomp/dom/DOMParserTest.java
@@ -0,0 +1,53 @@
+package com.bitcomp.dom;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+
+
+public class DOMParserTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp()
+    {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    public void parseXmlFile()
+    {
+        String source = "src/test/resources/source_data_aaa.xml";
+        String expected = "AREA_NUMBER/ flstkennz: 123456789_;" +
+                "AREA_SIZE/ flaeche: 2.00;" +
+                "LAND_NUMBER/ landschl: 05;" +
+                "PRECINCT_NUMBER/ kreisschl: 05378;" +
+                "DISTRICT_NUMBER/ gmdschl: 05378028;" +
+                "GEO_MARK_NUMBER/ gemaschl: 054533;";
+        try
+        {
+            DOMParser.parse(source, "aaa");
+            Assert.assertEquals(clearString(expected), clearString(outputStreamCaptor.toString()));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private String clearString(String str)
+    {
+        return str.toLowerCase(Locale.ROOT).replace(System.getProperty("line.separator"), "").trim();
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+}
